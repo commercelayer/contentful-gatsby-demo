dc129190ebd4112c5dcadb6977e908521902fb9d
Alessandro Casazza <alessandro@commercelayer.io>
2019-09-27 23:24:41 +0200

    Added apollo-react

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`, // or '.env'
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}` // or '.env'
 })
 
 module.exports = {
   siteMetadata: {
     title: `Gatsby Contentful Commerce`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@commercelayer`,
+    author: `@commercelayer`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,8 +14,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -28,22 +28,28 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     `gatsby-plugin-commercelayer`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-plugin-apollo',
+      options: {
+        uri: '/__graphql'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
