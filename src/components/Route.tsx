9db3ef73d4a5e7d9dc8d28cfe4ee2a8fab63f3ae
Alessandro Casazza <alessandro@commercelayer.io>
2019-10-07 17:42:56 +0200

    Removed unused file

--- src/components/Route.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { RouteProps } from '../types/index'
-
-const Route = ({ component: Component, ...rest }: RouteProps) => (
-  <Component {...rest} />
-)
-
-export default Route
