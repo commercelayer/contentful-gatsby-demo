46c571215f1ac214defb7dd6465efcfbf3c690df
Alessandro Casazza <alessandro@commercelayer.io>
2019-11-14 16:48:01 +0100

    feat: added product description

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -6,79 +6,79 @@ import { usePriceLoading } from '../hooks/index'
 import loader from '../images/three-dots-loader.svg'
 
 const Product = (props: ProductProps) => {
-	const { lang, data, onClick } = props
-	const loading = usePriceLoading('clayer-prices-ready')
-	const variants = data.variants.map(v => {
-		return {
-			code: v.code,
-			name: `${data.name} (${v.size.name})`,
-			label: v.size.name
-		}
-	})
-	const amountProps = {
-		amount: {
-			className: 'large has-text-success'
-		},
-		compare: {
-			className: 'large has-text-grey-light'
-		}
-	}
-	return (
-		<div className='columns'>
-			<div className='column is-two-thirds'>
-				<img src={data.image.file.url} alt={data.name} />
-			</div>
-			<div className='column'>
-				<h1 className='title'>{data.name}</h1>
+  const { lang, data, onClick } = props
+  const loading = usePriceLoading('clayer-prices-ready')
+  const variants = data.variants.map(v => {
+    return {
+      code: v.code,
+      name: `${data.name} (${v.size.name})`,
+      label: v.size.name
+    }
+  })
+  const amountProps = {
+    amount: {
+      className: 'large has-text-success'
+    },
+    compare: {
+      className: 'large has-text-grey-light'
+    }
+  }
+  return (
+    <div className='columns'>
+      <div className='column is-two-thirds'>
+        <img src={data.image.file.url} alt={data.name} />
+      </div>
+      <div className='column'>
+        <h1 className='title'>{data.name}</h1>
+        <p className='description'>{data.description.description}</p>
+        <div className='large'>
+          <CLayer.Price
+            skuCode={data.variants[0].code}
+            AmountProps={amountProps}
+          />
+          {loading ? <img src={loader} width='50' /> : null}
+        </div>
 
-				<div className='large'>
-					<CLayer.Price
-						skuCode={data.variants[0].code}
-						AmountProps={amountProps}
-					/>
-					{loading ? <img src={loader} width='50' /> : null}
-				</div>
+        <div className='select is-fullwidth variant-select-wrap'>
+          <CLayer.VariantSelect
+            className='variant-select'
+            PriceContainerId='price'
+            AvailabilityMessageContainerId='availability-message'
+            AddToBagId='add-to-bag'
+            promptText={locale[lang].select_size}
+            skus={variants}
+          />
+        </div>
+        <CLayer.AddToBag
+          className={`add-to-bag button is-success is-fullwidth`}
+          id='add-to-bag'
+          AvailabilityMessageContainerId='availability-message'
+          text={locale[lang].add_to_bag}
+          onClick={onClick}
+        />
 
-				<div className='select is-fullwidth variant-select-wrap'>
-					<CLayer.VariantSelect
-						className='variant-select'
-						PriceContainerId='price'
-						AvailabilityMessageContainerId='availability-message'
-						AddToBagId='add-to-bag'
-						promptText={locale[lang].select_size}
-						skus={variants}
-					/>
-				</div>
-				<CLayer.AddToBag
-					className={`add-to-bag button is-success is-fullwidth`}
-					id='add-to-bag'
-					AvailabilityMessageContainerId='availability-message'
-					text={locale[lang].add_to_bag}
-					onClick={onClick}
-				/>
+        <CLayer.AvailabilityMessageContainer id='availability-message' />
 
-				<CLayer.AvailabilityMessageContainer id='availability-message' />
-
-				<CLayer.AvailabilityMessageAvailableTemplate
-					className='available-message has-text-success'
-					availableTemplate={
-						<p className='has-text-success'>
-							<span className='is-capitalized'>
-								{locale[lang].available}
-							</span>{' '}
-							in{' '}
-							<CLayer.AvailabilityMessageMinDays className='available-message-min-days' />-<CLayer.AvailabilityMessageMaxDays className='available-message-max-days' />{' '}
-							{locale[lang].days}
-						</p>
-					}
-				/>
-				<CLayer.AvailabilityMessageUnavailableTemplate
-					className='unavailable-message has-text-danger'
-					unavailableTemplate={<p>{locale[lang].not_available}</p>}
-				/>
-			</div>
-		</div>
-	)
+        <CLayer.AvailabilityMessageAvailableTemplate
+          className='available-message has-text-success'
+          availableTemplate={
+            <p className='has-text-success'>
+              <span className='is-capitalized'>
+                {locale[lang].available}
+              </span>{' '}
+              in{' '}
+              <CLayer.AvailabilityMessageMinDays className='available-message-min-days' />-<CLayer.AvailabilityMessageMaxDays className='available-message-max-days' />{' '}
+              {locale[lang].days}
+            </p>
+          }
+        />
+        <CLayer.AvailabilityMessageUnavailableTemplate
+          className='unavailable-message has-text-danger'
+          unavailableTemplate={<p>{locale[lang].not_available}</p>}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Product
--- src/templates/ProductPage.tsx
+++ src/templates/ProductPage.tsx
@@ -7,76 +7,79 @@ import useShoppingBag from '../hooks'
 import SEO from '../components/seo'
 
 export default props => {
-	const {
-		pageContext: {
-			language,
-			shipping,
-			slug,
-			categorySlug,
-			categoryName,
-			pageTitle
-		},
-		data
-	} = props
-	const [status, setStatus] = useShoppingBag()
-	const delayTimer = useRef(null)
-	useEffect(() => {
-		return window.clearInterval(delayTimer.current)
-	})
-	const handleOnClick = e => {
-		if (e.target.hasAttribute('disabled')) {
-			return e.preventDefault()
-		}
-		delayTimer.current = window.setInterval(() => {
-			setStatus()
-		}, 1000)
-	}
-	return (
-		<Layout
-			{...props}
-			shoppingBagStatus={status}
-			setShoppingBagStatus={setStatus}
-		>
-			<SEO title={pageTitle} />
-			<Breadcrumb
-				shop={shipping.toLowerCase()}
-				lang={language}
-				uri={slug}
-				categorySlug={categorySlug}
-				categoryName={categoryName}
-				productSlug={data.contentfulProduct.name}
-			/>
-			<Product
-				shop={shipping.toLowerCase()}
-				lang={language}
-				data={data.contentfulProduct}
-				onClick={handleOnClick}
-			/>
-		</Layout>
-	)
+  const {
+    pageContext: {
+      language,
+      shipping,
+      slug,
+      categorySlug,
+      categoryName,
+      pageTitle
+    },
+    data
+  } = props
+  const [status, setStatus] = useShoppingBag()
+  const delayTimer = useRef(null)
+  useEffect(() => {
+    return window.clearInterval(delayTimer.current)
+  })
+  const handleOnClick = e => {
+    if (e.target.hasAttribute('disabled')) {
+      return e.preventDefault()
+    }
+    delayTimer.current = window.setInterval(() => {
+      setStatus()
+    }, 1000)
+  }
+  return (
+    <Layout
+      {...props}
+      shoppingBagStatus={status}
+      setShoppingBagStatus={setStatus}
+    >
+      <SEO title={pageTitle} />
+      <Breadcrumb
+        shop={shipping.toLowerCase()}
+        lang={language}
+        uri={slug}
+        categorySlug={categorySlug}
+        categoryName={categoryName}
+        productSlug={data.contentfulProduct.name}
+      />
+      <Product
+        shop={shipping.toLowerCase()}
+        lang={language}
+        data={data.contentfulProduct}
+        onClick={handleOnClick}
+      />
+    </Layout>
+  )
 }
 
 export const query = graphql`
-	query Product($productId: String, $language: String) {
-		contentfulProduct(
-			contentful_id: { eq: $productId }
-			node_locale: { eq: $language }
-			variants: { elemMatch: { node_locale: { eq: $language } } }
-		) {
-			name
-			image {
-				file {
-					url
-				}
-			}
-			reference
-			variants {
-				size {
-					name
-				}
-				name
-				code
-			}
-		}
-	}
+  query Product($productId: String, $language: String) {
+    contentfulProduct(
+      contentful_id: { eq: $productId }
+      node_locale: { eq: $language }
+      variants: { elemMatch: { node_locale: { eq: $language } } }
+    ) {
+      name
+      image {
+        file {
+          url
+        }
+      }
+      description {
+        description
+      }
+      reference
+      variants {
+        size {
+          name
+        }
+        name
+        code
+      }
+    }
+  }
 `
