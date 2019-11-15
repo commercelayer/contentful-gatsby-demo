a0bceb88753b65233e9128010f43c72ceed91c55
Alessandro Casazza <alessandro@commercelayer.io>
2019-11-15 18:44:29 +0100

    refactor: change filename

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,77 +1,77 @@
 require('dotenv').config({
-	path: `.env.${process.env.NODE_ENV}` // or '.env'
+  path: `.env.${process.env.NODE_ENV}` // or '.env'
 })
 
 module.exports = {
-	siteMetadata: {
-		title: `Contentful Gatsby Demo`,
-		description: ``,
-		author: `@commercelayer`
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`
-			}
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/images/favicon.png` // This path is relative to the root of the site.
-			}
-		},
-		{
-			resolve: `gatsby-source-contentful`,
-			options: {
-				spaceId: process.env.CONTENTFUL_SPACE_ID,
-				// Learn about environment variables: https://gatsby.dev/env-vars
-				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-			}
-		},
-		`gatsby-plugin-commercelayer`,
-		{
-			resolve: `gatsby-plugin-typescript`,
-			options: {
-				isTSX: true,
-				allExtensions: true
-			}
-		},
-		`gatsby-plugin-sass`
-		// {
-		// 	resolve: 'gatsby-plugin-apollo',
-		// 	options: {
-		// 		uri: 'https://cdn.contentful.com/content/v1/spaces/blh476ckz2yh',
-		// 		headers: {
-		// 			Authorization: `Bearer 0c74a3cd56e5d2c8eaf65da01c9c27e32babad0ff985abb3005812ccb713666c`
-		// 		}
-		// 	}
-		// },
-		// {
-		// 	resolve: `gatsby-plugin-create-client-paths`,
-		// 	options: { prefixes: [ `/us/*`, `/it/*` ] }
-		// }
-		// {
-		// 	resolve: `gatsby-plugin-i18next`,
-		// 	options: {
-		// 		availableLngs: [ 'en-us', 'it' ],
-		// 		fallbackLng: 'en-us',
-		// 		saveMissing: true,
-		// 		debug: true
-		// 	}
-		// }
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
-	]
+  siteMetadata: {
+    title: `Contentful Gatsby Demo`,
+    description: ``,
+    author: `@commercelayer`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN
+      }
+    },
+    `gatsby-plugin-commercelayer`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        allExtensions: true
+      }
+    },
+    `gatsby-plugin-sass`
+    // {
+    // 	resolve: 'gatsby-plugin-apollo',
+    // 	options: {
+    // 		uri: 'https://cdn.contentful.com/content/v1/spaces/blh476ckz2yh',
+    // 		headers: {
+    // 			Authorization: `Bearer 0c74a3cd56e5d2c8eaf65da01c9c27e32babad0ff985abb3005812ccb713666c`
+    // 		}
+    // 	}
+    // },
+    // {
+    // 	resolve: `gatsby-plugin-create-client-paths`,
+    // 	options: { prefixes: [ `/us/*`, `/it/*` ] }
+    // }
+    // {
+    // 	resolve: `gatsby-plugin-i18next`,
+    // 	options: {
+    // 		availableLngs: [ 'en-us', 'it' ],
+    // 		fallbackLng: 'en-us',
+    // 		saveMissing: true,
+    // 		debug: true
+    // 	}
+    // }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -9,9 +9,9 @@
 const path = require(`path`)
 
 exports.createPages = async ({ graphql, actions }) => {
-	const { createPage } = actions
-	// Catalogue
-	const result = await graphql(`
+  const { createPage } = actions
+  // Catalogue
+  const result = await graphql(`
     query {
 			allContentfulCountry {
 				edges {
@@ -36,58 +36,58 @@ exports.createPages = async ({ graphql, actions }) => {
 		}
   `)
 
-	result.data.allContentfulCountry.edges.forEach(({ node }) => {
-		// Catalogue page
-		createPage({
-			path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/`,
-			component: path.resolve(`./src/templates/CategoriesPage.tsx`),
-			context: {
-				// Data passed to context is available in page queries as GraphQL variables.
-				slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/`,
-				language: node.node_locale,
-				shipping: node.code,
-				pageTitle: node.node_locale === 'it' ? 'Categorie' : 'Categories'
-			}
-		})
-		node.catalogue.categories.map(c => {
-			const categorySlug = c.name
-				.trim()
-				.toLowerCase()
-				.replace(' & ', ' ')
-				.replace(/\s/gm, '-')
-			// Category page
-			createPage({
-				path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}`,
-				component: path.resolve(`./src/templates/ProductsPage.tsx`),
-				context: {
-					// Data passed to context is available in page queries as GraphQL variables.
-					slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}`,
-					language: node.node_locale,
-					shipping: node.code,
-					categoryId: c.contentful_id,
-					categorySlug,
-					pageTitle: c.name.trim()
-				}
-			})
-			c.products.map(p => {
-				const productSlug = p.name.trim().toLowerCase().replace(/\s/gm, '-')
-				// Product
-				createPage({
-					path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}/${productSlug}`,
-					component: path.resolve(`./src/templates/ProductPage.tsx`),
-					context: {
-						// Data passed to context is available in page queries as GraphQL variables.
-						slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}/${productSlug}`,
-						language: node.node_locale,
-						shipping: node.code,
-						categoryId: c.contentful_id,
-						categorySlug,
-						categoryName: c.name.trim(),
-						productId: p.contentful_id,
-						pageTitle: p.name.trim()
-					}
-				})
-			})
-		})
-	})
+  result.data.allContentfulCountry.edges.forEach(({ node }) => {
+    // Catalogue page
+    createPage({
+      path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/`,
+      component: path.resolve(`./src/templates/CatalogPage.tsx`),
+      context: {
+        // Data passed to context is available in page queries as GraphQL variables.
+        slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/`,
+        language: node.node_locale,
+        shipping: node.code,
+        pageTitle: node.node_locale === 'it' ? 'Categorie' : 'Categories'
+      }
+    })
+    node.catalogue.categories.map(c => {
+      const categorySlug = c.name
+        .trim()
+        .toLowerCase()
+        .replace(' & ', ' ')
+        .replace(/\s/gm, '-')
+      // Category page
+      createPage({
+        path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}`,
+        component: path.resolve(`./src/templates/CategoryPage.tsx`),
+        context: {
+          // Data passed to context is available in page queries as GraphQL variables.
+          slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}`,
+          language: node.node_locale,
+          shipping: node.code,
+          categoryId: c.contentful_id,
+          categorySlug,
+          pageTitle: c.name.trim()
+        }
+      })
+      c.products.map(p => {
+        const productSlug = p.name.trim().toLowerCase().replace(/\s/gm, '-')
+        // Product
+        createPage({
+          path: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}/${productSlug}`,
+          component: path.resolve(`./src/templates/ProductPage.tsx`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug: `/${node.code.toLowerCase()}/${node.node_locale.toLowerCase()}/${categorySlug}/${productSlug}`,
+            language: node.node_locale,
+            shipping: node.code,
+            categoryId: c.contentful_id,
+            categorySlug,
+            categoryName: c.name.trim(),
+            productId: p.contentful_id,
+            pageTitle: p.name.trim()
+          }
+        })
+      })
+    })
+  })
 }
