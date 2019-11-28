5b4462534b32ed35b277f66ef431d179d8b8de6d
Alessandro Casazza <alessandro@commercelayer.io>
2019-11-28 12:34:00 +0100

    feat: added pre-connect

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,12 @@ module.exports = {
     // }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: ['https://marvel.commercelayer.io']
+      }
+    }
   ]
 }
